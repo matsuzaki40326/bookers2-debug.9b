dbf7c4c82ff7f85978357f0bc27449fe18460528
matsu <98798507+matsuzaki40326@users.noreply.github.com>
2022-02-17 11:29:43 +0000

    [add]FFfunction

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,35 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :introduction, length: { maximum: 50 }
+
   has_many :books,dependent: :destroy
   has_many :favorites,dependent: :destroy
   has_many :post_comments,dependent: :destroy
 
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+
+  #フォローした時の処理
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+
+  #フォローを外した時の処理
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+
+  #フォローしているか判定
+  def following?(user)
+    followings.include?(user)
+  end
+
   has_one_attached :profile_image
-
-  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
-  validates :introduction, length: { maximum: 50 }
-
-
 
   def get_profile_image(width, height)
     unless profile_image.attached?
@@ -22,4 +41,5 @@ class User < ApplicationRecord
     end
     profile_image.variant(resize_to_limit: [width, height]).processed
   end
+
 end
