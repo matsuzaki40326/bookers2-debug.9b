b61be76295ed356f1a2b1f144e3c8ff7fc6e3f90
matsu <98798507+matsuzaki40326@users.noreply.github.com>
2022-02-20 10:31:10 +0000

    [add]いいね 非同期通信

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,7 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+require('jquery')
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
@@ -12,6 +13,7 @@ import "popper.js"
 import "bootstrap"
 import '@fortawesome/fontawesome-free/js/all';
 import "../stylesheets/application"
+
 
 Rails.start()
 Turbolinks.start()
