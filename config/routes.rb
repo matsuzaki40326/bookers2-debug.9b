ae1474b391d644a46ef022d1e4abb35a876b8767
matsu <98798507+matsuzaki40326@users.noreply.github.com>
2022-03-09 09:38:10 +0000

    [add]指定した日の投稿数を非同期で表示

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
    resource :relationships, only: [:create, :destroy]
    get 'followings' => 'relationships#followings', as: 'followings'
    get 'followers' => 'relationships#followers', as: 'followers'
+   get 'search', to: "users#search"
   end
 
   get 'chat/:id', to: 'chats#show', as: 'chat'
